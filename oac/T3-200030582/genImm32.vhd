3f7143051bad7f7e04710f751aa9a454d61c1cea
leopcortes <leo.pereira.cortes@gmail.com>
2023-11-11 19:15:58 -0300

    T3 OAC vf

--- oac/T3-200030582/genImm32.vhd
+++ oac/T3-200030582/genImm32.vhd
@@ -1,53 +1,56 @@
+library ieee;
+use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-package i_format is
-	type FORMAT_RV is (R_type, I_type, S_type, SB_type, UJ_type, U_type);
-end i_format;
-
-component genImm32 is
-	port (
-		instr : in  std_logic_vector(31 downto 0);
-		imm32 : out signed(31 downto 0));
-end component genImm32;
+entity genImm32 is
+  port (
+    instr : in  std_logic_vector(31 downto 0); -- Entrada
+    imm32 : out signed(31 downto 0) -- Saida
+  );
+end genImm32;
 
 architecture a of genImm32 is
-  	signal opcode : std_logic_vector(6 downto 0);
+  signal opcode : std_logic_vector(6 downto 0);
+  signal funct3 : std_logic_vector(14 downto 12);
 
 begin
-	opcode <= instr(6 downto 0);
-	funct3 <= instr(14 downto 12);
+  opcode <= instr(6 downto 0);
+  funct3 <= instr(14 downto 12);
 
-	with opcode select imm32 <=
-		-- R-Type:
-		resize("0", 32)
-		when x"33",
-
-		-- I-Type:
-		resize(signed(instr(31 downto 20)), 32)
-		when x"03" | x"13" | x"67",
-
-		-- I-Type*:
-		--resize(signed(instr(24 downto 20) & instr(31 downto 25) & '0' & instr(30 downto 25) & instr(11 downto 7)), 32)
-		resize(signed(instr(24 downto 20) & (others => '0')), 32)
-		when x"13" and funct3 = "101" and instr(30) = '1',
-
-		-- S-Type:
-		resize(signed(instr(31 downto 25) & instr(11 downto 7)), 32)
-		when x"23",
-
-		-- SB-Type:
-		resize(signed(instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8) & '0'), 32)
-		when x"63",
-
-		-- UJ-Type:
-		resize(signed(instr(31) & instr(19 downto 12) & instr(20) & instr(30 downto 21) & '0'), 32)
-		when x"37",
-
-		-- U-Type:
-		resize(signed(instr(31 downto 12) & x"000"), 32)
-		when x"6F",
-
-		-- Unknown:
-		resize("0", 32) when others;
-
-end genImm32;
+  process(opcode, funct3, instr)
+  begin
+  	-- R_type
+    if (opcode = "0110011") then
+      imm32 <= resize("0", 32);
+    
+    -- I_type*
+    elsif (opcode = "0010011" and funct3 = "101" and instr(30) = '1') then
+      imm32 <= resize(signed(instr(24 downto 20)), 32);
+    
+    -- I_type
+    elsif (opcode = "0000011" or opcode = "0010011" or opcode = "1100111") then
+      imm32 <= resize(signed(instr(31 downto 20)), 32);
+    
+    -- S_type
+    elsif (opcode = "0100011") then
+      imm32 <= resize(signed(instr(31 downto 25) & instr(11 downto 7)), 32);
+    
+    -- SB_type
+    elsif (opcode = "1100011") then
+      imm32 <= resize(signed(instr(31) & instr(7) & instr(30 downto 25) & instr(11 downto 8) & '0'), 32);
+    
+    -- UJ_type
+    elsif (opcode = "1101111") then
+      imm32 <= resize(signed(instr(31) & instr(19 downto 12) & instr(20) & instr(30 downto 21) & '0'), 32);
+    
+    -- U_type  
+    elsif (opcode = "0110111") then
+      imm32 <= resize(signed(instr(31 downto 12) & x"000"), 32);
+    
+    -- Unknown  
+    else
+      imm32 <= resize("0", 32);
+    end if;
+    
+  end process;
+end architecture a;
--- oac/T3-200030582/genImm32_tb.vhd
+++ oac/T3-200030582/genImm32_tb.vhd
@@ -6,67 +6,78 @@ entity genImm32_tb is
 end entity genImm32_tb;
 
 architecture a of genImm32_tb is
-    component genImm32 is
-        port (
-            instr : in  std_logic_vector(31 downto 0);
-            imm32 : out signed(31 downto 0));
-    end component genImm32;
+  component genImm32 is
+    port (
+      instr : in  std_logic_vector(31 downto 0); -- Entrada
+      imm32 : out signed(31 downto 0)); -- Saida
+  end component genImm32;
 
-    signal instr_in  : std_logic_vector(31 downto 0);
-    signal imm32_out : signed(31 downto 0);
+  signal instr_in  : std_logic_vector(31 downto 0); -- Entrada
+  signal imm32_out : signed(31 downto 0); -- Saida
 
 begin
-    gerador : genImm32 port map(instr_in, imm32_out);
+  gen : genImm32 port map(instr_in, imm32_out);
 
-    process
-    begin
-        instr_in <= x"000002b3";
-        wait for 1 ns;
-        assert(imm32_out = x"00000000") report "Error Teste 1" severity error;
+  process
+  begin
+    -- Teste 1: 0x000002b3 -> 0x00000000
+    instr_in <= x"000002b3";
+    wait for 1 ns;
+    assert(imm32_out = x"00000000") report "Erro no Teste 1" severity error;
 
-        instr_in <= x"01002283";
-        wait for 1 ns;
-        assert(imm32_out = x"00000010") report "Error Teste 2" severity error;
+    -- Teste 2: 0x01002283 -> 0x00000010
+    instr_in <= x"01002283";
+    wait for 1 ns;
+    assert(imm32_out = x"00000010") report "Erro no Teste 2" severity error;
 
-        instr_in <= x"f9c00313";
-        wait for 1 ns;
-        assert(imm32_out = x"FFFFFF9C") report "Error Teste 3" severity error;
+    -- Teste 3: 0xF9C00313 -> 0xFFFFFF9C
+    instr_in <= x"f9c00313";
+    wait for 1 ns;
+    assert(imm32_out = x"FFFFFF9C") report "Erro no Teste 3" severity error;
 
-        instr_in <= x"fff2c293";
-        wait for 1 ns;
-        assert(imm32_out = x"FFFFFFFF") report "Error Teste 4" severity error;
+    -- Teste 4: 0xFFF2C293 -> 0xFFFFFFFF
+    instr_in <= x"fff2c293";
+    wait for 1 ns;
+    assert(imm32_out = x"FFFFFFFF") report "Erro no Teste 4" severity error;
 
-        instr_in <= x"16200313";
-        wait for 1 ns;
-        assert(imm32_out = x"00000162") report "Error Teste 5" severity error;
+    -- Teste 5: 0x16200313 -> 0x00000162
+    instr_in <= x"16200313";
+    wait for 1 ns;
+    assert(imm32_out = x"00000162") report "Erro no Teste 5" severity error;
 
-        instr_in <= x"01800067";
-        wait for 1 ns;
-        assert(imm32_out = x"00000018") report "Error Teste 6" severity error;
+    -- Teste 6: 0x01800067 -> 0x00000018
+    instr_in <= x"01800067";
+    wait for 1 ns;
+    assert(imm32_out = x"00000018") report "Erro no Teste 6" severity error;
 
-        instr_in <= x"40a3d313";
-        wait for 1 ns;
-        assert(imm32_out = x"0000000A") report "Error Teste 7" severity error;
+    -- Teste 7: 0x40A3D313 -> 0x0000000A
+	instr_in <= x"40a3d313";
+  	wait for 1 ns;
+    assert(imm32_out = x"0000000A") report "Erro no Teste 7" severity error;
 
-        instr_in <= x"00002437";
-        wait for 1 ns;
-        assert(imm32_out = x"00002000") report "Error Teste 8" severity error;
+    -- Teste 8: 0x00002437 -> 0x00002000
+    instr_in <= x"00002437";
+    wait for 1 ns;
+    assert(imm32_out = x"00002000") report "Erro no Teste 7" severity error;
 
-        instr_in <= x"02542e23";
-        wait for 1 ns;
-        assert(imm32_out = x"0000003C") report "Error Teste 9" severity error;
+    -- Teste 9: 0x02542E23 -> 0x0000003C
+    instr_in <= x"02542e23";
+    wait for 1 ns;
+    assert(imm32_out = x"0000003C") report "Erro no Teste 8" severity error;
 
-        instr_in <= x"fe5290e3";
-        wait for 1 ns;
-        assert(imm32_out = x"FFFFFFE0") report "Error Teste 10" severity error;
+    -- Teste 10: 0xFE5290E3 -> 0xFFFFFFE0
+    instr_in <= x"fe5290e3";
+    wait for 1 ns;
+    assert(imm32_out = x"FFFFFFE0") report "Erro no Teste 9" severity error;
 
-        instr_in <= x"00c000ef";
-        wait for 2 ns;
-        assert(imm32_out = x"0000000C") report "Error Teste 11" severity error;
+    -- Teste 11: 0x00C000EF -> 0x0000000C
+    instr_in <= x"00c000ef";
+    wait for 2 ns;
+    assert(imm32_out = x"0000000C") report "Erro no Teste 10" severity error;
 
-        assert false report "Test done." severity note;
+    assert false report "Testes concluidos com sucesso." severity note;
 
-        wait;
+    wait;
 
-    end process;
+  end process;
 end a;
