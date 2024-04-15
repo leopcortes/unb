70e1e30bb334829762e767fb05e4e8a75a42f469
leopcortes <leo.pereira.cortes@gmail.com>
2024-04-15 09:39:29 -0300

    mp trabalho1 v1

---
+++ mp/trabalho1/README.md
@@ -0,0 +1,5 @@
+# Executando o projeto
+
+Para compilar o programa basta estar no diretório pai (trabalho1) e rodar o comando `make`
+
+Será gerado um arquivo executável `romano`, e para testar uma conversão de romano para decimal, basta rodar o comando `./romano <string_numero_romano>`
---
+++ mp/trabalho1/src/romano.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+#include "romano.h"
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+  if (argc < 2) {
+    cout << "Erro ao executar, utilização: " << argv[0] << " <string>" << endl;
+    return -1;  
+  }
+
+  cout << "Número romano original: " << argv[1] << endl;
+
+  cout << "Número romano convertido: " << converter_romano(argv[1]) << endl;
+
+  return 0;
+}
