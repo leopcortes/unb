e3fb2a526852de64cf45e3b1749ce5f1c37f9e4d
leopcortes <leo.pereira.cortes@gmail.com>
2024-04-19 15:11:37 -0300

    mp t1 v7

--- mp/trabalho1/romanos.hpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2024 - Leonardo P Cortes (200030582)
-#ifndef MP_TRABALHO1_ROMANOS_HPP_
-#define MP_TRABALHO1_ROMANOS_HPP_
-int romanos_para_decimal(char const * num_romano);
-#endif  // MP_TRABALHO1_ROMANOS_HPP_
